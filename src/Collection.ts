0cfd0f888657eba4e129dad362b6705376f4e341
Glowney <bglowney@amazon.com>
2017-06-09 08:06:33 -0700

    Apply new onaddcallback to existing collection elements

--- src/Collection.ts
+++ src/Collection.ts
@@ -7,14 +7,23 @@ import {Component} from "./Component";
 
 export class Collection extends Component {
 
-    children<M>(model: ModelCollection<M,any>, onAddCallback: UpdateCallback<ModelElement<M>,AbstractComponent>): this {
-        model.registerAddCallback(this, function (newItem: ModelElement<any>) {
-            let i = model instanceof ModelArray ? (model as ModelArray<any>).size.get(): "";
+    children<M>(model: ModelCollection<M,Object>, onAddCallback: UpdateCallback<ModelElement<M>,AbstractComponent>): this {
+        model.registerAddCallback(this, addItem.bind(this));
+
+        function addItem (newItem: ModelElement<any>, k?: any) {
+            let i = k || (model instanceof ModelArray ? (model as ModelArray<any>).size.get(): "");
             let newComponent = onAddCallback(newItem, i);
             newItem.bindComponent(newComponent);
             // this.element.appendChild(newComponent.getElement());
             this.child(newComponent);
-        }.bind(this));
+        }
+
+        // we should also apply the callback to any existing elements in the collection
+        for (let k in model.get()) {
+            let v = model.get()[k];
+            addItem.call(this, v, k)
+        }
+
         return this;
     }
 
