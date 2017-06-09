d1daf16be649f12af18541fdfd11fe916d777066
Glowney <bglowney@amazon.com>
2017-06-09 08:05:38 -0700

    Apply new onaddcallback to existing collection elements

--- test/TestApp.bundled.js
+++ test/TestApp.bundled.js
@@ -408,12 +408,17 @@ const ModelArray_1 = require("./ModelArray");
 const Component_1 = require("./Component");
 class Collection extends Component_1.Component {
     children(model, onAddCallback) {
-        model.registerAddCallback(this, function (newItem) {
-            let i = model instanceof ModelArray_1.ModelArray ? model.size.get() : "";
+        model.registerAddCallback(this, addItem.bind(this));
+        function addItem(newItem, k) {
+            let i = k || (model instanceof ModelArray_1.ModelArray ? model.size.get() : "");
             let newComponent = onAddCallback(newItem, i);
             newItem.bindComponent(newComponent);
             this.child(newComponent);
-        }.bind(this));
+        }
+        for (let k in model.get()) {
+            let v = model.get()[k];
+            addItem.call(this, v, k);
+        }
         return this;
     }
 }
